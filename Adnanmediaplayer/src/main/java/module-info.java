72a7d94be9f904aed6419619147cd8fa06b18ade
Esteak Ahmed Adnan <96421546+Adnan-Ahmed07@users.noreply.github.com>
2024-12-17 01:10:51 -0700

    have any error working on it

--- Adnanmediaplayer/src/main/java/module-info.java
+++ Adnanmediaplayer/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.example.myapp {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
 
     opens org.example.myapp to javafx.fxml;
     exports org.example.myapp;
--- Adnanmediaplayer/src/main/java/org/example/myapp/MediaPlayer.java
+++ Adnanmediaplayer/src/main/java/org/example/myapp/MediaPlayer.java
@@ -1,14 +1,56 @@
 package org.example.myapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class MediaPlayer {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnPlay;
+
+    @FXML
+    private Label lblDuration;
+
+    @FXML
+    private MediaView mediaView;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    @FXML
+    void btnPlay(MouseEvent event) {
+
     }
+
+    @FXML
+    void btnStop(MouseEvent event) {
+
+    }
+
+    @FXML
+    void selectMedia(ActionEvent event) {
+        FileChooser fileChooser =new FileChooser();
+        fileChooser.setTitle("Select Media");
+        File selectedFile= fileChooser.showOpenDialog(null);
+
+        if(selectedFile !=null){
+            String url=selectedFile.toURI().toString();
+
+            media =new Media(url);
+            mediaPlayer= new MediaPlayer(media);
+
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.setAutoPlay(true);
+        }
+    }
+
 }
